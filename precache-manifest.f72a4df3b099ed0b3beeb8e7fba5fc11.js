07a78f37d30fe471f67c6ed362e5c1b70eab4b27
manjil thapa <manjilthapa@manjils-MacBook-Pro.local>
2020-01-22 22:20:43 +0100

    Updates

---
+++ precache-manifest.f72a4df3b099ed0b3beeb8e7fba5fc11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53cc06ee70f2b0eb17ab6a12f25fdc9",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cc750fccbad2b33954ad",
+    "url": "/react-monsters-rolodex/static/css/main.def1f5e3.chunk.css"
+  },
+  {
+    "revision": "f9e22d7cc6c4c351561a",
+    "url": "/react-monsters-rolodex/static/js/2.0b9f1bf0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters-rolodex/static/js/2.0b9f1bf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc750fccbad2b33954ad",
+    "url": "/react-monsters-rolodex/static/js/main.f83711d9.chunk.js"
+  },
+  {
+    "revision": "3376963b2391a0e2e1a6",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.44f38db4.js"
+  }
+]);
